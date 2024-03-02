669dadd3f51396807f3559c2aaeba5170737b6d9
Abdalrahman-Muhammad <abdelrhmanmohamed421@gmail.com>
2024-03-02 11:04:17 +0200

    enhance accordion functionality

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,14 +1,19 @@
+import { useState } from 'react';
 import AccordionItem from './AccordionItem';
 const Accordion = ({ data }) => {
+  const [curOpen, setCurOpen] = useState(null);
   return (
     <div className='accordion'>
       {data.map((ele, i) => (
         <AccordionItem
           key={ele.title}
           title={ele.title}
-          text={ele.text}
           number={i + 1}
-        />
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+        >
+          {ele.text}
+        </AccordionItem>
       ))}
     </div>
   );
